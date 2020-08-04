395d7c3281535bd786c0e057b16b62866196b76c
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-04 09:43:38 +0800

    Update easy.cpp

--- easy.cpp
+++ easy.cpp
@@ -2,45 +2,49 @@
 
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void test_case() {
 	string s;
 	getline(cin, s);
 	int n = (int) s.size();
-	bool p[n + 1];
+	bool p[n+1];
 	memset(p, true, sizeof(p));
-	for (int i = 0; i < n; i++) {
-		if (isspace(s[i]) || ispunct(s[i])) {
-			p[i - 1] = false;
+	for(int i = 0; i < n; ++i) {
+		if(isspace(s[i]) || ispunct(s[i])) {
+			p[i-1] = false;
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (p[i] == true) {
-				if (isupper(s[i]) && isupper(s[j])) {
+	for(int i = 0; i < n; ++i) {
+		for(int j = i + 1; j < n; ++j) {
+			if(p[i] == true) {
+				if(isupper(s[i]) && isupper(s[j])) {
 					p[j] = false;
 					swap(s[i], s[j]);
-					if (j == i + 1) {
-						i++;
-					}
+					if(j == i + 1) i++;
 					break;
-				} else if (islower(s[i]) && islower(s[j])) {
+				}
+				else if(islower(s[i]) && islower(s[j])) {
 					p[j] = false;
 					swap(s[i], s[j]);
-					if (j == i + 1) {
-						i++;
-					}
+					if(j == i + 1) i++;
 					break;
 				}
 			}
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		if (isalpha(s[i])) {
-			s[i] -= 3;
-		}
+	for(int i = 0; i < n; ++i){ 
+		if(isalpha(s[i])) s[i] -= 3;
 	}
-	cout << s << '\n';
+	cout << s << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//int T;
+	//cin >> T;
+	//for(int nr = 1; nr <= T; ++nr) {
+		//test_case();
+	//}
+	test_case();
 	return 0;
 }
